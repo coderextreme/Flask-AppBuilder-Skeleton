4d1f1c86b14a7456aea19bca03064a19ab2f2dc8
John Carlson <yottzumm@gmail.com>
2019-12-27 22:24:12 -0600

    made no registration necessary

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ CSRF_ENABLED = True
 # AUTH_ROLE_ADMIN = 'Admin'
 
 # Uncomment to setup Public role name, no authentication needed
-# AUTH_ROLE_PUBLIC = 'Public'
+AUTH_ROLE_PUBLIC = 'Public'
 
 # Will allow user self registration
 # AUTH_USER_REGISTRATION = True
